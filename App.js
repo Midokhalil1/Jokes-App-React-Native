715e5a47df238fa47ac6f8441d3fa1bc331ee151
Midokhalil1 <midokhalil22@gmail.com>
2022-11-08 16:24:29 -0500

    adding jokes

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  View } from 'react-native';
+import styles from './styles';
+// import jokes Punchine, and other componantes 
+import Joke from './components/Joke';
+import Punchline from './components/Punchline';
 
 export default function App() {
+  // create state to hold jokes 
+  const [jokeList, setJokeList] = useState()
+  // create state to hold last joke index
+  const [currentJokeIndex, setCurrentJokeIndex] = useState(0)
+  // create function to get another 
+  const getNextJoke = () => {
+    if(currentJokeIndex < jokeList.length - 1) {
+
+      setCurrentJokeIndex(currentJokeIndex + 1)
+
+    }else {
+      setCurrentJokeIndex(0)
+    }
+  }
+  // useEffect to get all jokes 
+  useEffect(() => {
+    fetch('https://api.sampleapis.com/jokes/goodJokes')
+    .then(result => result.json())
+    .then(setJokeList)
+    .catch(alert)
+  }, [])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Joke joke={jokeList[currentJokeIndex].setup} />
+      <Punchline punchline={jokeList[currentJokeIndex].punchline} />
+      
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
---
+++ styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+const styles= StyleSheet.create({
+
+        container: {
+          flex: 1,
+          backgroundColor: '#ff00ff',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+jokeText: {
+    textAlign: 'center',
+    fontSize: 40,
+    
+
+},
+punchlines: {
+    textAlign: 'center',
+    color: '#c00',
+    fontSize: 32,
+    
+   
+},
+
+})
+
+export default styles;
