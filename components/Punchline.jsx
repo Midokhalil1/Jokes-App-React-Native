becaf8a443941f7973387dc440c58555be8152b6
Midokhalil1 <midokhalil22@gmail.com>
2022-11-13 13:04:43 -0500

    usestate

--- components/Punchline.jsx
+++ components/Punchline.jsx
@@ -1,7 +1,12 @@
+import { useState, useEffect } from 'react'
+
 import { Text } from 'react-native'
+
+
 import styles from '../styles'
 
 export default function Punchline({ punchline }) {
+    const [reveal, setReveal]= useState(false)
     return (
         <Text style={styles.punchline}>{punchline}</Text>
     )
