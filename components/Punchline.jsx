1c7094e9d7b4b70105a119358125f4334b59d274
Midokhalil1 <midokhalil22@gmail.com>
2022-11-13 13:07:47 -0500

    updated

--- components/Punchline.jsx
+++ components/Punchline.jsx
@@ -7,7 +7,9 @@ import styles from '../styles'
 
 export default function Punchline({ punchline }) {
     const [reveal, setReveal]= useState(false)
+    useEffect(() => {
     useEffect(false)
+}, [punchline])
     return (
         <Text style={styles.punchline}>{punchline}</Text>
     )
